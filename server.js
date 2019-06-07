2c67b6472d7ef2f6f5090fa7d6dedc930f3f3056
Gilmore <Sean.Gilmore@utsad.com>
2019-06-07 09:58:22 -0400

    Added Service route for ping, removed sample handler

--- server.js
+++ server.js
@@ -74,36 +74,35 @@ var unifiedServer = function( req, res ) {
             'headers' : headers,
             'payload' : buffer
         };
-    });
 
-    // Route the request to the handler specified in the router
-    chosenHandler( data, function( statusCode, payload ) {
-        // Use the status code called back by the handler, of default to 200
-        statusCode = typeof( statusCode ) == 'number' ? statusCode : 200;
+        // Route the request to the handler specified in the router
+        chosenHandler( data, function( statusCode, payload ) {
+            // Use the status code called back by the handler, of default to 200
+            statusCode = typeof( statusCode ) == 'number' ? statusCode : 200;
 
-        // Use the payload called back by the handler, or default to an empty object
-        payload = typeof( payload ) == 'object' ? payload : {};
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof( payload ) == 'object' ? payload : {};
 
-        // Convert the payload to a string
-        var payloadString = JSON.stringify( payload );
+            // Convert the payload to a string
+            var payloadString = JSON.stringify( payload );
 
-        // Return response
-        res.setHeader( 'Content-Type', 'application/json' );
-        res.writeHead( statusCode );
-        res.end( payloadString );
+            // Return response
+            res.setHeader( 'Content-Type', 'application/json' );
+            res.writeHead( statusCode );
+            res.end( payloadString );
 
-        // Log request path
-        consonle.log( 'Returning this response: ', statusCode, payloadString );
+            // Log request path
+            console.log( 'Returning this response: ', statusCode, payloadString );
+        });
     });
 };
 
 // Define handlers
 var handlers = {};
 
-// Sample handler
-handlers.sample = function ( data, callback ) {
-    // Callback an http status code, and a payload object
-    callback( 406, { 'name' : 'sample handler' } );
+// Ping handler
+handlers.ping = function( data, callback ) {
+    callback( 200 );
 };
 
 // Handler not found
@@ -113,5 +112,5 @@ handlers.notFound = function ( data, callback ) {
 
 // Define a request router
 var router = {
-    'sample' : handlers.sample
+    'ping' : handlers.ping
 };
